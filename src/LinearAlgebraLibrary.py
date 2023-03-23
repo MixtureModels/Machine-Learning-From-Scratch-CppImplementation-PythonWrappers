434a7b560b9773cce1718b409349a39c64a03757
MixtureModels <Sammyhl3027@gmail.com>
2023-03-23 02:34:08 -0400

    refactoring, new languages added

--- src/LinearAlgebraLibrary.py
+++ src/LinearAlgebraLibrary.py
@@ -4,10 +4,13 @@ import subprocess
 from stopwatch import Stopwatch
 
 class LinearAlgebraLibraryCpp:
+    """
+    Class for C++ implementation of Linear Algebra Library Python wrapper
+    """
     def __init__(self):
-        self.compile_script_path = "../Scripts/startup.sh"
+        self.compile_script_path = "../Scripts/startupCpp.sh"
         self.cpp_compile_library(self.compile_script_path)
-        self.linalg_lib_path = pathlib.Path().absolute() / "Shared/linalg.so"
+        self.linalg_lib_path = pathlib.Path().absolute() / "Shared/linalgCpp.so"
         self.linalg = ctypes.CDLL(self.linalg_lib_path)
 
     def cpp_compile_library(self, file_path):
@@ -15,27 +18,41 @@ class LinearAlgebraLibraryCpp:
         subprocess.run([compile_script_path], shell=True)
         print('<< script to compile cpp-files ran')
 
-    # python wrapper with cpp-implementation: add vectors
+    # Python wrapper with cpp-implementation: add vectors
     def add_vectors(self, vec_one, vec_two, size):
         array_type = ctypes.c_double * 3
         int_type = ctypes.c_int
         self.linalg.addVectors(array_type(*vec_one), array_type(*vec_two), int_type(size))
 
-    # helper to benchmark speed-comparison between python-native and cpp-compiled library function
+    # Helper to benchmark speed-comparison between python-native and cpp-compiled library function
     def speed_test_helper(self, vec_one, vec_two, size, iterations):
         array_type = ctypes.c_double * size
         int_type = ctypes.c_int
         self.linalg.speedTestHelper(array_type(*vec_one), array_type(*vec_two), int_type(iterations), int_type(size))
 
+class LinearAlgebraLibraryC:
+    """
+    Class for vanilla C-language implementation of Linear Algebra Library python wrapper
+    """
+    def __init__(self):
+        self.compile_script_path = "../Scripts/startupC.sh"
+        self.cpp_compile_library(self.compile_script_path)
+        self.linalg_lib_path = pathlib.Path().absolute() / "Shared/linalgC.so"
+        self.linalg = ctypes.CDLL(self.linalg_lib_path)
 
 class LinearAlgebraLibraryPython:
-    
+    """
+    Class for Python implementation of Linear Algebra Library Python wrapper
+    """   
     def speed_test_helper(self, vec_one, vec_two, size, iterations):
         for i in range(iterations):
             for j in range(size):
                 computedVal = vec_one[j] + vec_two[j]
 
 class LinearAlgebraLibraryRust:
+    """
+    Class for Rust implementation of Linear Algebra Library Python wrapper
+    """   
     def __init__(self):
         self.compile_script_path = "../Scripts/startup.sh"
         self.cpp_compile_library(self.compile_script_path)
